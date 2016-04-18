ddf63c2aeb329cb985647d2ed4eca2261265f8ab
Markos Horro Varela <marcos.horro@udc.es>
2016-04-18 14:00:50 +0200

    Cleaning code, making new structure of directory

---
+++ lib/colors.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// more details http://ascii-table.com/ansi-escape-sequences.php
+
+#define RES  "\x1B[0m"
+#define BLD  "\x1B[1m"
+#define RED  "\x1B[31m"
+#define GRN  "\x1B[32m"
+#define YEL  "\x1B[33m"
+#define BLU  "\x1B[34m"
+#define MAG  "\x1B[35m"
+#define CYN  "\x1B[36m"
+#define WHT  "\x1B[37m"
---
+++ lib/utils.h
@@ -0,0 +1,16 @@
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <getopt.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <vector>
+#include <math.h>
+#include "colors.h"
+#include "isa.h"
+#include "copy.h"
+#include "rapidxml.hpp"
+#include "rapidxml_print.hpp"
