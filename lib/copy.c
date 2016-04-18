5e725500920a8288821f87583136b496a7f981bd
Markos Horro Varela <marcos.horro@udc.es>
2016-04-18 10:23:26 +0200

    Added function to copy a file

---
+++ lib/copy.c
@@ -0,0 +1,48 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void copy(char *source, char *dest)
+{
+    int childExitStatus;
+    pid_t pid;
+    int status;
+    char *error_msg = "Error copying template! Quitting...\n";
+    if (!source || !dest) {
+        /* handle as you wish */
+    }
+
+    pid = fork();
+
+    if (pid == 0) { /* child */
+        execl("/bin/cp", "/bin/cp", source, dest, (char *)0);
+    }
+    else if (pid < 0) {
+        /* error - couldn't start process */
+        printf("%s", error_msg);
+	exit(-1);
+    }
+    else {
+        /* parent - wait for child - this has all error handling, you
+         * could just call wait() as long as you are only expecting to
+         * have one child process at a time.
+         */
+        pid_t ws = waitpid( pid, &childExitStatus, WNOHANG);
+        if (ws == -1)
+        { /* error */
+	    printf("%s", error_msg);
+	    exit(-1);
+        }
+
+        if( WIFEXITED(childExitStatus)) /* exit code in childExitStatus */
+        {
+            status = WEXITSTATUS(childExitStatus); /* zero is normal exit */
+        }
+        else if (WIFSIGNALED(childExitStatus)) /* killed */
+        {
+        }
+        else if (WIFSTOPPED(childExitStatus)) /* stopped */
+        {
+        }
+    }
+}
---
+++ lib/copy.h
@@ -0,0 +1,4 @@
+// taken from https://stackoverflow.com/questions/2180079/how-can-i-copy-a-file-on-unix-using-c
+#include "copy.c"
+
+void copy(char *source, char *dest);
