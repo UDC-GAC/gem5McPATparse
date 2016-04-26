6e4e2b3e75c4f390a96db7ef9e58d46078d62cf3
Markos Horro Varela <marcos.horro@udc.es>
2016-04-26 12:30:08 +0200

    Updated licence headers in all sources

--- lib/handle_options.h
+++ lib/handle_options.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2016 Universidade da Coruña
- * All rights reserved.
+ * All rights reserved. MIT Licence
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
